0a29201db40de7a304e8ea92f1d6733ab3e95da4
Iyke <204012532+Iykesan@users.noreply.github.com>
2025-12-31 18:16:51 +0900

    initial draft, basics working

--- scripts.js
+++ scripts.js
@@ -14,19 +14,73 @@ const divide = (a, b) => {
     return a / b
 }
 
-const num1     = null;
-const operator = null;
-const num2     = null;
+let numberInput = [];
+let numberInput2 = [];
+let num1        = null;
+let operator    = '';
+let num2        = null;
+let isSecondNumber = false
+let result = null;
 
 const operate = (x, o, y) => {
-    o(x, y)
+   switch(o){
+    case "+":
+        return add(x, y)
+        break;
+
+    case "-":
+        return subtract(x, y)
+        break;
+    
+    case "*":
+        return multiply(x, y)
+        break;
+    
+    case "/":
+        return divide(x, y)
+        break;
+    default:
+        return "try again..."
+    }   
 }
+const buttons       = document.querySelectorAll("#button")
+const display       = document.querySelector("#display");
+const allButton     = document.querySelectorAll("button");
+const clear         = document.querySelector("#clear")
+const allOperators  = document.querySelectorAll(".operator")
+const allNumbers    = document.querySelectorAll(".numbers")
+const equal         = document.querySelector("#equal")
 
-const buttons = document.querySelectorAll("#button")
-const display = document.quesrySelector("#display")
-
-buttons.addEventListener('click', (event) =>{
-    const numbers = document.createElement("p")
-    numbers.textContent = event.value
-    display.appendChild(numbers)
+allButton.forEach(button =>{
+    button.addEventListener('click', e =>{
+        const number = document.createElement("p")
+        number.textContent = e.target.value
+        display.appendChild(number)
+        if(isSecondNumber){
+            numberInput2.push(e.target.value)
+        }else{
+            numberInput.push(e.target.value)
+        }
+    })
 })
+
+allOperators.forEach(button =>{
+    button.addEventListener('click', e =>{  
+        if(isSecondNumber){
+            num2 = Number(numberInput2.slice(0, -1).join(''))
+            display.textContent = result = operate(num1, operator, num2)
+        }
+        operator = numberInput.at(-1)
+        num1 = Number(numberInput.slice(0, -1).join(''))
+        isSecondNumber = true
+    })
+})
+
+equal.addEventListener('click', e =>{
+    num2 = Number(numberInput2.slice(0, -1).join(''))
+    display.textContent = result = operate(num1, operator, num2)
+})
+
+clear.addEventListener('click', e =>
+    window.location.reload() 
+)
